7fed38709adf77840075e09a103839b693246f8f
MNnazrul <mnnazrulislam196130@gmail.com>
2025-10-26 23:35:16 +0600

    remn-editor

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+mod editor;
+use editor::Editor;
+fn main() {
+    // let mut editor = Editor::default();
+    // editor.run();
+    Editor::default().run();
+}
